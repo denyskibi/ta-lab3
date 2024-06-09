cdf16da4d9a0be75d9fe10a1e841472870a1eb85
Denys K <github@kibitkin.com>
2024-06-09 18:04:03 +0300

    Developed quick sort and iterations count for it

---
+++ core/quick_sort.py
@@ -0,0 +1,35 @@
+# Standard Libraries
+from typing import List
+
+
+class QuickSort:
+    def __init__(self):
+        self._total_comparisons: int = 0
+
+    def sort(self, array: List[int], lo: int, hi: int) -> None:
+        if lo < hi:
+            # Performing the separation procedure and getting the position of the reference element and
+            # the number of comparisons
+            p = self._split_array_into_parts(array, lo, hi)
+
+            self.sort(array, lo, p - 1)  # recursive sorting the left part of the array
+            self.sort(array, p + 1, hi)  # recursive sorting the right part of the array
+
+    def get_total_comparisons(self) -> int:
+        return self._total_comparisons
+
+    def _split_array_into_parts(self, array: List[int], lo: int, hi: int) -> int:
+        i = lo - 1  # start from minus one index to calculate swaps ??
+        pivot = array[hi]  # setting the last element as a reference
+
+        # Selection of elements and their comparison with the reference element
+        for j in range(lo, hi):
+            self._total_comparisons += 1
+            if array[j] <= pivot:  # if the current is less than or equal to the reference
+                i += 1
+                array[i], array[j] = array[j], array[i]  # swap elements
+
+        # Placing a reference element between smaller and larger ones
+        array[i + 1], array[hi] = array[hi], array[i + 1]
+
+        return i + 1
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 # Custom Modules
+from core.quick_sort import QuickSort
 from utils import file_utils
 
 
@@ -10,16 +11,22 @@ def stop():
 
 
 def main():
+    # Create necessary class objects
+    quick_sort = QuickSort()
+
     try:
         # Step #1: Load list from file
         loaded_num_list = file_utils.load_list_from_txt_file(file_path="files/input_10000_numbers.txt")
 
-        ...
+        # Step #2: Call quick sort function
+        loaded_numbers_count = len(loaded_num_list)
+        quick_sort.sort(array=loaded_num_list, lo=0, hi=loaded_numbers_count - 1)
+
+        # Step #3: Get & print the total comparisons number
+        total_comparisons = quick_sort.get_total_comparisons()
+        print(f"[INFO] The total number of compilations using quick sort method: {total_comparisons}")
     except KeyboardInterrupt:
         print("[ERROR] Failed: script interrupted by user (CTRL + C)")
-        stop()
-    except Exception as e:
-        print(f"[ERROR] Failed due to error: {e}")
         stop()
 
 
